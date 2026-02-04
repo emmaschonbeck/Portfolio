69df62b7ad9b29c2774ee7821897e7b3f5d7aeaf
Emma <emmaschonbeck7@gmail.com>
2026-02-04 14:24:31 +0100

    Initial commit, setup next.js with Tailwind.

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+export const Header = (props: Props) => {
+  return (
+    <header className='flex flex-col md:flex-row justify-between items-center p-4'>
+      
+    </header>
+  )
+}
